42027b04ec2347ff8739ecd48ca16fe5b8448fe1
RuslanPro01 <ruslan91999@gmail.com>
2022-09-28 15:36:03 +0300

    Создаёт функцию, возвращающую случайное нецелое число из переданного диапазона включительно

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
  * @param maxNumber {number} конец диапазона
  * @return {number} рандомное число
  */
-const getRandomNumber = (minNumber, maxNumber) => {
+const getRandomNumber = (minNumber = 0, maxNumber = 0) => {
   if (minNumber > maxNumber) {
     const mindNumber = minNumber;
     minNumber = maxNumber;
@@ -13,4 +13,23 @@ const getRandomNumber = (minNumber, maxNumber) => {
   return Math.floor(Math.random() * (maxNumber + 1 - minNumber)) + minNumber;
 };
 
-console.log(getRandomNumber(0, 2));
+getRandomNumber(0, 2);
+
+/**
+ * Функция, возвращающая случайное нецелое число из переданного диапазона включительно
+ * @param minNumber {number} начало диапазона
+ * @param maxNumber {number} конец диапазона
+ * @param precisionNumber {number} переменная, которая определяет количество знаков после запятой
+ * @return {number} рандомное число с плавающей точкой округленное до precisionNumber
+ */
+const getRandomFloatNumber = (minNumber = 0, maxNumber = 0, precisionNumber = 0) => {
+  if (minNumber > maxNumber) {
+    const mindNumber = minNumber;
+    minNumber = maxNumber;
+    maxNumber = mindNumber;
+  }
+  const randomNumberStreet = Math.random() * (maxNumber + 1 - minNumber) + minNumber;
+  return Number(randomNumberStreet.toFixed(precisionNumber));
+};
+
+getRandomFloatNumber(0, 2, 2);
