3b338b2c16247c062fa3483ad9e7f168e73bd511
RuslanPro01 <ruslan91999@gmail.com>
2022-11-14 21:35:37 +0300

    Удаляет неиспользуемый код

--- js/utils.js
+++ js/utils.js
@@ -1,45 +1,5 @@
 import {POP_UP_CLOSING_TIME} from './contants.js';
 /**
- * Функция, возвращающая случайное целое число из переданного диапазона включительно
- * @param minNumber {number} начало диапазона
- * @param maxNumber {number} конец диапазона
- * @return {number} рандомное число
- */
-const getRandomNumber = (minNumber = 0, maxNumber = 0) => {
-  if (minNumber > maxNumber) {
-    const mindNumber = minNumber;
-    minNumber = maxNumber;
-    maxNumber = mindNumber;
-  }
-  return Math.floor(Math.random() * (maxNumber + 1 - minNumber)) + minNumber;
-};
-
-
-/**
- * Функция, возвращающая случайное нецелое число из переданного диапазона включительно
- * @param minNumber {number} начало диапазона
- * @param maxNumber {number} конец диапазона
- * @param precisionNumber {number} переменная, которая определяет количество знаков после запятой
- * @return {number} рандомное число с плавающей точкой округленное до precisionNumber
- */
-const getRandomFloatNumber = (minNumber = 0, maxNumber = 0, precisionNumber = 0) => {
-  if (minNumber > maxNumber) {
-    const mindNumber = minNumber;
-    minNumber = maxNumber;
-    maxNumber = mindNumber;
-  }
-  const randomNumber = Math.random() * (maxNumber + 1 - minNumber) + minNumber;
-  return Number(randomNumber.toFixed(precisionNumber));
-};
-
-/**
- * Возвращает случайный элемент переданного массива
- * @param array {array} массив из которого нужно вернуть случайное значк=ение
- * @return {*} случайный элемент массива
- */
-const getRandomArrayElement = (array) => array[getRandomNumber(0, array.length - 1)];
-
-/**
  * Сопостовляет слово с ключами объекта и возвращает значение совпавшего ключа
  * @param listTranslatedWords {Object} перебираемый объект
  * @param word {string} слово
@@ -90,9 +50,6 @@ const compareArrays = (arrayA, arrayB) => arrayA.length >= arrayB.length && arra
 const isEscape = (evt) => evt.key === 'Escape';
 
 export {
-  getRandomNumber,
-  getRandomFloatNumber,
-  getRandomArrayElement,
   translateWord,
   editTemplate,
   showAlert,
